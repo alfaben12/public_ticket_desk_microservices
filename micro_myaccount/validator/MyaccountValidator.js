a0bad178267a4a7f8583a7ab501145c6f725eaa7
Thariqalfa <zrav420@gmail.com>
2019-03-27 16:42:15 +0700

    update validator body

--- micro_myaccount/validator/MyaccountValidator.js
+++ micro_myaccount/validator/MyaccountValidator.js
@@ -7,7 +7,7 @@ exports.processSetupMyaccountValidation = function(req, res, next) {
 	req.checkBody('txt_cities_id', 'Cities is required').notEmpty();
 	req.checkBody('txt_first_name', 'First Name is required').notEmpty();
 	req.checkBody('txt_last_name', 'Last Name is required').notEmpty();
-	req.checkBody('txt_phone', 'Phone is required').notEmpty();
+	req.checkBody('txt_phone', 'Phone is required').isLength({ min: 9, max: 13 });
 	req.checkBody('txt_birth', 'Birth is required').notEmpty();
 	req.checkBody('txt_address', 'Address is required').notEmpty();
 
@@ -20,10 +20,10 @@ exports.processSetupMyaccountValidation = function(req, res, next) {
 };
 
 exports.processSetupCCValidation = function(req, res, next) {
-	req.checkBody('txt_card_number', 'Card Number is require').notEmpty();
+	req.checkBody('txt_card_number', 'Card Number is require').isLength({ min: 14, max: 16 });
 	req.checkBody('txt_month', 'Month is require').notEmpty();
 	req.checkBody('txt_year', 'Year is require').notEmpty();
-	req.checkBody('txt_cvv', 'Cvv is require').notEmpty();
+	req.checkBody('txt_cvv', 'Cvv is require').isLength({ min: 3, max: 4 });
 	req.checkBody('txt_card_holder', 'Cardholder is require').notEmpty();
 	req.checkBody('txt_scheme', 'Scheme is require').notEmpty();
 
